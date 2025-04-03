eb92036fbc781396e3e8f2459874c3c6108432b8
Bao Nguyen <baonguyen@Baos-MacBook-Air-3.local>
2025-04-03 14:06:56 +0700

    update latest

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,45 +2,44 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  const testTitle = "SATMOCKS Test 2 Module 2";
-  const answers = "BDAADDDBCBBBACDCBCBAADCBCCCBABBDB";
-  /** 
   await prisma.user.createMany({
     data: [
-      { username: 'minhduc', password: '12345' },
-      { username: 'dieulinh', password: '12345' },
-      { username: 'david', password: '12345' },
+      { username: 'minhtrang', password: '12345' },
+      { username: 'chauanh', password: '12345' },
     ],
   });
-*/
-  const user = await prisma.user.findUnique({
-    where: { username: "minhduc" },
-  });
 
-  if (!user) {
-    throw new Error("User 'minhduc' not found!");
-  }
-  const test = await prisma.test.create({
-    data: {
-      title: testTitle,
-      questions: {
-        create: answers.split('').map((answer, i) => ({
-          number: i + 1,
-          correctAnswer: answer
-        }))
-      }
-    },
-  });
-  await prisma.userTest.create({
-    data: {
-      userId: user.id,
-      testId: test.id,
-      status: 'UNFINISHED',
-      timeTaken: 60, // 45 minutes
-      completedAt: new Date('2025-03-27T00:00:00Z'),
-    },
-  });
-
+  /** 
+  const testTitle = "SATMOCKS Test 2 Module 2";
+  const answers = "BDAADDDBCBBBACDCBCBAADCBCCCBABBDB";
+    const user = await prisma.user.findUnique({
+      where: { username: "minhduc" },
+    });
+  
+    if (!user) {
+      throw new Error("User 'minhduc' not found!");
+    }
+    const test = await prisma.test.create({
+      data: {
+        title: testTitle,
+        questions: {
+          create: answers.split('').map((answer, i) => ({
+            number: i + 1,
+            correctAnswer: answer
+          }))
+        }
+      },
+    });
+    await prisma.userTest.create({
+      data: {
+        userId: user.id,
+        testId: test.id,
+        status: 'UNFINISHED',
+        timeTaken: 60, // 45 minutes
+        completedAt: new Date('2025-03-27T00:00:00Z'),
+      },
+    });
+    */
   console.log('Users and Tests seeded!');
 }
 
